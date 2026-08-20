aa6ffb71be1f30c57aa834158ff421f069878bfe
johalvme1 <143954117+johalvme1@users.noreply.github.com>
2026-08-19 19:28:53 -0600

    Revert "Ocultar solo retos vencidos por tiempo (no por desactivación)"
    This reverts commit 463b1fa6aa63e4ae14d2c80b44e3455d11b12c4f.

--- backend/challenges/serializers.py
+++ backend/challenges/serializers.py
@@ -31,7 +31,11 @@ class ChallengeSerializer(serializers.ModelSerializer):
         if is_supervisor_user(request.user):
             return False
         now = timezone.now()
-        if not obj.end_date or now <= obj.end_date:
+        started = (not obj.start_date) or obj.start_date <= now
+        if not started:
+            return False
+        finished = (obj.end_date and now > obj.end_date) or not obj.active
+        if not finished:
             return False
         subs = obj.submissions.filter(user=request.user)
         if not subs.exists():
