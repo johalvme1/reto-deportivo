0932ad66bc96c7c320713b56f70ff70d6a1a6af7
johalvme1 <143954117+johalvme1@users.noreply.github.com>
2026-09-01 20:03:56 -0600

    fix: import duplicado en views.py

--- backend/points/views.py
+++ backend/points/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.views import APIView
 from django.db.models import Count, Q
-        from .models import DailyPoint, RestDay, CompetitionPeriod, Measurement, MeasurementSchedule, MeasurementSchedule
+from .models import DailyPoint, RestDay, CompetitionPeriod, Measurement, MeasurementSchedule
 from .serializers import DailyPointSerializer
 from activities.models import Activity
 from challenges.models import ChallengeSubmission, Challenge, Medal
